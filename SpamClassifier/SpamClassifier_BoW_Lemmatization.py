e41d7458363f670aa891e7aec69e2f03a7b34588
Mostafa Mohiuddin Jalal Protik <75379858+protikmostafa083@users.noreply.github.com>
2021-09-15 00:04:02 +0600

    Add files via upload

---
+++ SpamClassifier/SpamClassifier_BoW_Lemmatization.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 22:17:27 2021
+
+@author: PROTIK
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 14 10:32:57 2021
+
+@author: PROTIK
+"""
+
+''''
+USING LEMMATIZATION
+'''
+
+
+
+# Import and read data from dataset
+
+import pandas as pd
+
+messages = pd.read_csv('smsspamcollection/SMSSpamCollection', sep='\t', names=['label', 'message'])
+
+
+# Data cleaning and Preprocessing
+import re                                                 # Regular Expression
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+# Creating Stemming Object
+lemmatization = WordNetLemmatizer()               
+cleanedSentence = []
+
+# Cleaning Process
+for i in range(0, len(messages)):
+    sents = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    sents = sents.lower()
+    sents = sents.split()
+    sents = [lemmatization.lemmatize(word) for word in sents if word not in set(stopwords.words('english'))]
+    sents = ' '.join(sents)
+    cleanedSentence.append(sents)
+    
+# implementing Bag of Words Models
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 5000)                  # max_feature = top number of columns I want to choose
+X = cv.fit_transform(cleanedSentence).toarray()
+
+
+# Creating Dummy Variable for another column - Class
+y = pd.get_dummies(messages['label'])
+y = y.iloc[:, 1].values
+
+
+# Train-Test Split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2, random_state=0)
+
+
+# Training Naive Bayes Classifier
+from sklearn.naive_bayes import MultinomialNB
+spam_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred = spam_model.predict(X_test)
+
+
+# Comparing
+from sklearn.metrics import confusion_matrix
+conf_mat = confusion_matrix(y_test, y_pred)
+
+#accuracy score
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_test, y_pred)
+
+print(acc*100)
+
+
+
+
+
+
+
+
+
+
+
